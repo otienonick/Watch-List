d8131c368cbd2d9c9e01635a9c46802af0d9df14
otienonick <nick.odongi@student.moringaschool.com>
2021-09-08 17:21:12 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ class Config:
     '''
     MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
     SECRET_KEY = os.urandom(32)
 
 
---
+++ start.sh
@@ -0,0 +1,4 @@
+ export MOVIE_API_KEY=4fd793215300450014ee89ee14945659
+export  SECRET_KEY = os.urandom(32)
+
+python3.6 manage.py server

f44985ca380defa56805318df659133eeacc50c8
otienonick <nick.odongi@student.moringaschool.com>
2021-09-15 23:51:15 +0300

    pushed to heroku

--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ from app.models import User,Role,Review
 
 
 # creating app instance
-app = create_app('production')
+app = create_app('development')
+app = create_app('test')
 
 
 migrate = Migrate(app,db)
